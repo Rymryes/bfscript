158d5c7d19fa4543bc9b02195d920571334c4f36
Rymryes <117486649+Rymryes@users.noreply.github.com>
2023-03-26 12:20:19 +0300

    Update

--- BFScript.lua
+++ BFScript.lua
@@ -1,60 +1,65 @@
--- Configuration
-local aimAssistDistance = 20 -- adjust this value to control the distance at which aim assist is active
+local aiming = false -- whether or not aim assist is currently enabled
+local aimButton = script.Parent -- the UI button that toggles aim assist
 
--- Create the button
-local button = Instance.new("TextButton")
-button.Text = "Open"
-button.Size = UDim2.new(0, 100, 0, 50)
-button.Position = UDim2.new(0, 10, 0, 10)
-button.BackgroundTransparency = 0.5
-button.Parent = game.Players.LocalPlayer.PlayerGui
+-- function to enable aim assist
+local function enableAimAssist()
+    aiming = true
+    aimButton.Text = "Disable Aim Assist"
+end
 
--- Create the aim assist function
-local function aimAssist()
-    -- Get the player's camera and character
-    local camera = game.Workspace.CurrentCamera
-    local character = game.Players.LocalPlayer.Character
-    if not camera or not character then
-        return
+-- function to disable aim assist
+local function disableAimAssist()
+    aiming = false
+    aimButton.Text = "Enable Aim Assist"
+end
+
+-- connect the button to the enable/disable functions
+aimButton.Activated:Connect(function()
+    if aiming then
+        disableAimAssist()
+    else
+        enableAimAssist()
     end
+end)
+
+-- main loop to update aim assist
+while true do
+    wait(0.1) -- adjust this value to control how frequently the aim is updated
     
-    -- Get the closest NPC or player within the aim assist distance
-    local closestObject, closestDistance
-    for _, object in ipairs(game.Workspace:GetChildren()) do
-        if object:IsA("Model") and (object.Name == "NPC" or object:IsDescendantOf(game.Players)) then
-            local distance = (object.PrimaryPart.Position - camera.CFrame.Position).magnitude
-            if distance <= aimAssistDistance and (not closestDistance or distance < closestDistance) then
-                closestObject = object
-                closestDistance = distance
+    if aiming then
+        local player = game.Players.LocalPlayer
+        local character = player.Character
+        local mouse = player:GetMouse()
+        local target
+        
+        -- detect if the player is targeting an NPC or another player
+        local hit = mouse.Hit
+        local hitPart = hit and hit.Instance and hit.Instance.Parent
+        if hitPart and hitPart:IsA("Model") then
+            if hitPart:FindFirstChild("Humanoid") then
+                -- targeting an NPC
+                target = hitPart
+            end
+        else
+            -- targeting a player
+            local targetCharacter = workspace:FindFirstChild(hitPart.Name)
+            if targetCharacter and targetCharacter ~= character then
+                target = targetCharacter
+            end
+        end
+        
+        -- lock on to the target if one is found
+        if target then
+            local targetPart = target:FindFirstChild("HumanoidRootPart")
+            if targetPart then
+                -- aim at the target
+                local direction = (targetPart.Position - character.HumanoidRootPart.Position).unit
+                character.HumanoidRootPart.CFrame = CFrame.new(character.HumanoidRootPart.Position, targetPart.Position) * CFrame.new(0, 0, -10)
+                
+                -- lock on to the target
+                character.Humanoid:ChangeState(Enum.HumanoidStateType.Swimming)
+                character.Humanoid:ChangeState(Enum.HumanoidStateType.Physics)
             end
         end
     end
-    
-    -- Aim at the closest NPC or player
-    if closestObject then
-        local target = closestObject.PrimaryPart.Position
-        local targetScreenPos = camera:WorldToScreenPoint(target)
-        local screenSize = game:GetService("GuiService"):GetScreenResolution()
-        local aimPos = Vector2.new(targetScreenPos.X, targetScreenPos.Y)
-        local aimCenter = screenSize / 2
-        local aimDelta = (aimPos - aimCenter) / screenSize
-        character.Humanoid.AutoRotate = false
-        character.HumanoidRootPart.CFrame = CFrame.new(camera.CFrame.Position, camera.CFrame.Position + (camera.CFrame.LookVector + Vector3.new(aimDelta.x, 0, aimDelta.y)).unit * 1000)
-    else
-        character.Humanoid.AutoRotate = true
-    end
 end
-
--- Hook up the button
-local isOpen = false
-button.MouseButton1Click:Connect(function()
-    isOpen = not isOpen
-    button.Text = isOpen and "Off" or "Open"
-    if isOpen then
-        button.BackgroundColor3 = Color3.new(0, 1, 0)
-        aimAssistConnection = game:GetService("RunService").Heartbeat:Connect(aimAssist)
-    else
-        button.BackgroundColor3 = Color3.new(1, 0, 0)
-        aimAssistConnection:Disconnect()
-    end
-end)
