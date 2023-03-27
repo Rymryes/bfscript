2392fe9460539ac099db2a1877e2ae497e4e2f3c
Rymryes <117486649+Rymryes@users.noreply.github.com>
2023-03-27 07:33:56 +0300

    Update 9.8

--- BFScript.lua
+++ BFScript.lua
@@ -1,62 +1,46 @@
---// Find the nearest NPC or player with the specified color
-function FindTarget(Color)
+--// SETTINGS \\
+local Settings = {
+    AimAtNPCs = true, -- Set to true to aim at NPCs
+    AimAtPlayers = true, -- Set to true to aim at players
+    MaxDistance = 1000, -- Maximum distance to target (in studs)
+    TargetPartName = "Head", -- Part to aim at
+}
+
+--// MAIN FUNCTION \\
+local function GetClosestTarget()
     local Target = nil
-    local TargetDist = math.huge
-    
+    local ClosestDistance = Settings.MaxDistance
     for _, Character in pairs(game:GetService("Players"):GetPlayers()) do
-        if Character ~= game:GetService("Players").LocalPlayer then
-            local Humanoid = Character.Character:FindFirstChildOfClass("Humanoid")
-            
-            if Humanoid and Humanoid.Health > 0 then
-                local Head = Character.Character:FindFirstChild("Head")
-                
-                if Head then
-                    local HeadColor = Head.BillboardGui.Frame.ImageColor3
-                    
-                    if HeadColor == Color then
-                        local TargetMagnitude = (Head.Position - game:GetService("Players").LocalPlayer.Character.Head.Position).magnitude
-                        
-                        if TargetMagnitude < TargetDist then
-                            Target = Character.Character
-                            TargetDist = TargetMagnitude
-                        end
-                    end
+        if Character.Character and Character.Character:FindFirstChild("HumanoidRootPart") and Character.Character:FindFirstChild(Settings.TargetPartName) then
+            local Distance = (Character.Character.HumanoidRootPart.Position - game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position).magnitude
+            if Distance < ClosestDistance and Distance <= Settings.MaxDistance and Character.Character.Humanoid.Health > 0 then
+                Target = Character.Character:FindFirstChild(Settings.TargetPartName)
+                ClosestDistance = Distance
+            end
+        end
+    end
+    if Settings.AimAtNPCs then
+        for _, NPC in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
+            if NPC:FindFirstChild("HumanoidRootPart") and NPC:FindFirstChild(Settings.TargetPartName) then
+                local Distance = (NPC.HumanoidRootPart.Position - game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position).magnitude
+                if Distance < ClosestDistance and Distance <= Settings.MaxDistance and NPC.Humanoid.Health > 0 then
+                    Target = NPC:FindFirstChild(Settings.TargetPartName)
+                    ClosestDistance = Distance
                 end
             end
         end
     end
-    
-    for _, NPC in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
-        local Humanoid = NPC:FindFirstChildOfClass("Humanoid")
-        
-        if Humanoid and Humanoid.Health > 0 then
-            local Head = NPC:FindFirstChild("Head")
-            
-            if Head then
-                local HeadColor = Head.BillboardGui.Frame.ImageColor3
-                
-                if HeadColor == Color then
-                    local TargetMagnitude = (Head.Position - game:GetService("Players").LocalPlayer.Character.Head.Position).magnitude
-                    
-                    if TargetMagnitude < TargetDist then
-                        Target = NPC
-                        TargetDist = TargetMagnitude
-                    end
-                end
-            end
-        end
-    end
-    
     return Target
 end
 
---// Set up the aimbot
-local AimbotColor = Color3.fromRGB(255, 0, 0) -- Replace with your desired color
+local function AimAt(Target)
+    if not Target then return end
+    local Camera = workspace.CurrentCamera
+    local TargetPosition = Camera:WorldToViewportPoint(Target.Position)
+    mousemoverel((TargetPosition.X - mouse.X) * 3, (TargetPosition.Y - mouse.Y) * 3)
+end
 
+--// MAIN LOOP \\
 while wait() do
-    local Target = FindTarget(AimbotColor)
-    
-    if Target then
-        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = Target.HumanoidRootPart.CFrame
-    end
+    AimAt(GetClosestTarget())
 end
