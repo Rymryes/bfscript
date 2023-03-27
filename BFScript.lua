d31fe07b5d6ef7d1f98c77843149976c35eb0b91
Rymryes <117486649+Rymryes@users.noreply.github.com>
2023-03-27 07:11:43 +0300

    Update

--- BFScript.lua
+++ BFScript.lua
@@ -1,72 +1,62 @@
--- Define the settings
-local settings = {
-    targetColors = {
-        Color3.fromRGB(255, 0, 0), -- Red
-        Color3.fromRGB(0, 255, 0), -- Green
-        Color3.fromRGB(0, 0, 255)  -- Blue
-    },
-    fov = 70, -- Field of view
-    maxDistance = 1000 -- Maximum distance to target
-}
-
--- Define the user interface
-local ui = Instance.new("ScreenGui")
-ui.Name = "AimbotUI"
-ui.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
-
-local toggleButton = Instance.new("TextButton")
-toggleButton.Name = "ToggleButton"
-toggleButton.Text = "Aimbot: OFF"
-toggleButton.Size = UDim2.new(0, 100, 0, 30)
-toggleButton.Position = UDim2.new(0.5, -50, 0.9, 0)
-toggleButton.Parent = ui
-
--- Define the aimbot function
-local function aimbot()
-    -- Get the player's camera and character
-    local camera = workspace.CurrentCamera
-    local character = game:GetService("Players").LocalPlayer.Character
-
-    -- Get all NPCs and players within the field of view and maximum distance
-    local targets = {}
-    for _, target in ipairs(workspace:GetDescendants()) do
-        if target:IsA("Model") and target:FindFirstChild("Humanoid") and target:FindFirstChild("HumanoidRootPart") then
-            local targetPosition = target.HumanoidRootPart.Position
-            local screenPosition, onScreen = camera:WorldToScreenPoint(targetPosition)
-            if onScreen and screenPosition.Z > 0 and screenPosition.Z < camera.ViewportSize.Z then
-                local distance = (targetPosition - character.HumanoidRootPart.Position).magnitude
-                if distance <= settings.maxDistance then
-                    for _, color in ipairs(settings.targetColors) do
-                        if target.Humanoid.RootPart.Color == color then
-                            table.insert(targets, {target = target, distance = distance})
-                            break
+--// Find the nearest NPC or player with the specified color
+function FindTarget(Color)
+    local Target = nil
+    local TargetDist = math.huge
+    
+    for _, Character in pairs(game:GetService("Players"):GetPlayers()) do
+        if Character ~= game:GetService("Players").LocalPlayer then
+            local Humanoid = Character.Character:FindFirstChildOfClass("Humanoid")
+            
+            if Humanoid and Humanoid.Health > 0 then
+                local Head = Character.Character:FindFirstChild("Head")
+                
+                if Head then
+                    local HeadColor = Head.BillboardGui.Frame.ImageColor3
+                    
+                    if HeadColor == Color then
+                        local TargetMagnitude = (Head.Position - game:GetService("Players").LocalPlayer.Character.Head.Position).magnitude
+                        
+                        if TargetMagnitude < TargetDist then
+                            Target = Character.Character
+                            TargetDist = TargetMagnitude
                         end
                     end
                 end
             end
         end
     end
-
-    -- Find the closest target and aim at it
-    if #targets > 0 then
-        table.sort(targets, function(a, b) return a.distance < b.distance end)
-        local closestTarget = targets[1].target
-        local lookVector = (closestTarget.HumanoidRootPart.Position - character.HumanoidRootPart.Position).unit
-        camera.CFrame = CFrame.new(camera.CFrame.Position, camera.CFrame.Position + (lookVector * settings.maxDistance))
+    
+    for _, NPC in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
+        local Humanoid = NPC:FindFirstChildOfClass("Humanoid")
+        
+        if Humanoid and Humanoid.Health > 0 then
+            local Head = NPC:FindFirstChild("Head")
+            
+            if Head then
+                local HeadColor = Head.BillboardGui.Frame.ImageColor3
+                
+                if HeadColor == Color then
+                    local TargetMagnitude = (Head.Position - game:GetService("Players").LocalPlayer.Character.Head.Position).magnitude
+                    
+                    if TargetMagnitude < TargetDist then
+                        Target = NPC
+                        TargetDist = TargetMagnitude
+                    end
+                end
+            end
+        end
     end
+    
+    return Target
 end
 
--- Connect the toggle button to the aimbot function
-local isAimbotEnabled = false
-toggleButton.MouseButton1Click:Connect(function()
-    isAimbotEnabled = not isAimbotEnabled
-    if isAimbotEnabled then
-        toggleButton.Text = "Aimbot: ON"
-        while isAimbotEnabled do
-            aimbot()
-            wait()
-        end
-    else
-        toggleButton.Text = "Aimbot: OFF"
+--// Set up the aimbot
+local AimbotColor = Color3.fromRGB(255, 0, 0) -- Replace with your desired color
+
+while wait() do
+    local Target = FindTarget(AimbotColor)
+    
+    if Target then
+        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = Target.HumanoidRootPart.CFrame
     end
-end)
+end
