71b126c12560eeb680f3ec66684e5789f0ee6562
Rymryes <117486649+Rymryes@users.noreply.github.com>
2023-03-27 07:46:13 +0300

    Update 1.0

--- BFScript.lua
+++ BFScript.lua
@@ -1,46 +1,20 @@
---// SETTINGS \\
-local Settings = {
-    AimAtNPCs = true, -- Set to true to aim at NPCs
-    AimAtPlayers = true, -- Set to true to aim at players
-    MaxDistance = 1000, -- Maximum distance to target (in studs)
-    TargetPartName = "Head", -- Part to aim at
-}
+local fpsUnlocker = Instance.new("ScreenGui")
+local fpsLabel = Instance.new("TextLabel")
 
---// MAIN FUNCTION \\
-local function GetClosestTarget()
-    local Target = nil
-    local ClosestDistance = Settings.MaxDistance
-    for _, Character in pairs(game:GetService("Players"):GetPlayers()) do
-        if Character.Character and Character.Character:FindFirstChild("HumanoidRootPart") and Character.Character:FindFirstChild(Settings.TargetPartName) then
-            local Distance = (Character.Character.HumanoidRootPart.Position - game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position).magnitude
-            if Distance < ClosestDistance and Distance <= Settings.MaxDistance and Character.Character.Humanoid.Health > 0 then
-                Target = Character.Character:FindFirstChild(Settings.TargetPartName)
-                ClosestDistance = Distance
-            end
-        end
-    end
-    if Settings.AimAtNPCs then
-        for _, NPC in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
-            if NPC:FindFirstChild("HumanoidRootPart") and NPC:FindFirstChild(Settings.TargetPartName) then
-                local Distance = (NPC.HumanoidRootPart.Position - game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position).magnitude
-                if Distance < ClosestDistance and Distance <= Settings.MaxDistance and NPC.Humanoid.Health > 0 then
-                    Target = NPC:FindFirstChild(Settings.TargetPartName)
-                    ClosestDistance = Distance
-                end
-            end
-        end
-    end
-    return Target
-end
+fpsUnlocker.Parent = game:GetService("CoreGui")
+fpsUnlocker.Name = "FPS Unlocker"
 
-local function AimAt(Target)
-    if not Target then return end
-    local Camera = workspace.CurrentCamera
-    local TargetPosition = Camera:WorldToViewportPoint(Target.Position)
-    mousemoverel((TargetPosition.X - mouse.X) * 3, (TargetPosition.Y - mouse.Y) * 3)
-end
+fpsLabel.Parent = fpsUnlocker
+fpsLabel.Name = "FPSLabel"
+fpsLabel.BackgroundColor3 = Color3.new(1, 1, 1)
+fpsLabel.BackgroundTransparency = 1
+fpsLabel.Position = UDim2.new(0, 0, 0, 0)
+fpsLabel.Size = UDim2.new(0, 100, 0, 20)
+fpsLabel.Font = Enum.Font.SourceSansBold
+fpsLabel.TextColor3 = Color3.new(1, 1, 1)
+fpsLabel.TextSize = 20
+fpsLabel.Text = "FPS: "
 
---// MAIN LOOP \\
-while wait() do
-    AimAt(GetClosestTarget())
-end
+game:GetService("RunService").Stepped:Connect(function()
+    fpsLabel.Text = "FPS: " .. tostring(math.floor(1 / game:GetService("RunService").RenderStepped:Wait()))
+end)
