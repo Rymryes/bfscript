269bbd9c683f0081932e40195a49126b7235a905
Rymryes <117486649+Rymryes@users.noreply.github.com>
2023-03-29 17:41:21 +0300

    Update BFScript.lua

--- BFScript.lua
+++ BFScript.lua
@@ -1,1 +1,2 @@
-
+if not game:GetService("UserInputService").TouchEnabled and not game:GetService("UserInputService").KeyboardEnabled == false then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Rymryes/TheFinder./main/TheFinder.lua"))()
